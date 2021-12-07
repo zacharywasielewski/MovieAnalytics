fdeff5d546be7cb944c5e9294e6155224c0458e7
Zachary Wasielewski <84246587+zacharywasielewski@users.noreply.github.com>
2021-12-06 21:37:50 -0500

    Update Movie DF Clean.py

--- Movie DF Clean.py
+++ Movie DF Clean.py
@@ -1,36 +1,29 @@
 import pandas as pd
 
-
-#encode categorical variables
-dummy_cols = ['Languages','Genres','Directors','Producers','Executive_Producers',
-              'Writers','Composers','Cinematographers','Top_10_Cast']
-movie_dataframe2 = movie_dataframe #pull movie_dataframe from other script
-for col in dummy_cols:
-    s = movie_dataframe2[col]
-    temp = pd.get_dummies(s.apply(pd.Series).stack(), prefix=col).sum(level=0)
-    movie_dataframe2 = pd.concat([movie_dataframe2, temp], axis=1)
-    movie_dataframe2 = movie_dataframe2[[x for x in movie_dataframe2.columns if x != col]]
-
-#subset encoded columns to only columns where sum >= 3
-df_sum = movie_dataframe2.sum(numeric_only = True)
-df_rem_list = list(df_sum[df_sum < 3].index) 
-movie_dataframe3 = movie_dataframe2[[x for x in movie_dataframe2.columns if x not in df_rem_list]]
-
-#remove genders 'list' columns
-movie_dataframe3 = movie_dataframe3[[x for x in movie_dataframe3.columns if x[-8:] != '_Genders']]
-
-#remove misc vars (unused for now!)
-movie_dataframe3 = movie_dataframe3[[x for x in movie_dataframe3.columns if x not in ['Release_Date','Watch_Date','Overview']]]
-
-#assign index as Movie
-movie_dataframe3 = movie_dataframe3.set_index('Movie')
-
-#fill nulls
-movie_dataframe3 = movie_dataframe3.fillna(0)
-
-
-
-#Hayley code -remove later
-#movie_dataframe3.loc[(movie_dataframe3['Genres_Comedy'] == 1) & (movie_dataframe3['Top_Cast_Popularity_Avg']<1000000)][['Rating','Vote_Avg']].describe()
-
-print(movie_dataframe.loc[movie_dataframe['Overview'].str.contains('boo',case=False)][['Overview']])
+if __name__ == "__main__":
+    #encode categorical variables
+    dummy_cols = ['Languages','Genres','Directors','Producers','Executive_Producers',
+                  'Writers','Composers','Cinematographers','Top_10_Cast']
+    df_dummy = df #pull movie_dataframe from other script
+    for col in dummy_cols:
+        s = df_dummy[col]
+        temp = pd.get_dummies(s.apply(pd.Series).stack(), prefix=col).sum(level=0)
+        df_dummy = pd.concat([df_dummy, temp], axis=1)
+        df_dummy = df_dummy[[x for x in df_dummy.columns if x != col]]
+    
+    #subset encoded columns to only columns where sum >= 3
+    df_sum = df.sum(numeric_only = True)
+    df_rem_list = list(df_sum[df_sum < 3].index) 
+    df_dummy_sub = df_dummy[[x for x in df_dummy.columns if x not in df_rem_list]]
+    
+    #remove genders 'list' columns
+    df_dummy_sub = df_dummy_sub[[x for x in df_dummy_sub.columns if x[-8:] != '_Genders']]
+    
+    #remove misc vars (unused for now!)
+    df_dummy_sub = df_dummy_sub[[x for x in df_dummy_sub.columns if x not in ['Release_Date','Watch_Date','Overview']]]
+    
+    #assign index as Movie
+    df_dummy_sub = df_dummy_sub.set_index('Movie')
+    
+    #fill nulls
+    df_final = df_dummy_sub.fillna(0)
